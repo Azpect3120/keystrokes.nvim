828fd8907b4bc6e98e13bb9af2ec6f25674d57a3
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-02-04 22:46:05 -0700

    Moved window

--- lua/keystrokes/init.lua
+++ lua/keystrokes/init.lua
@@ -7,24 +7,25 @@
 -- returned when the module is called with `require`.
 local M = {}
 
+-- Create a window for the keystrokes
 function M.CreateWindow ()
   -- nvim_create_buf({listed}, {scratch}): buffer handler OR 0 on error
   local buf = vim.api.nvim_create_buf(false, true)
 
   -- nvim_open_win(){buffer}, {enter}, {*config}): window handler OR 0 on error
   local win = vim.api.nvim_open_win(buf, false, {
-    relative="editor",    -- creates float when specified
-    style="minimal",      -- remove the normal vim setup
-    border="rounded",     -- options: "single", "double", "shadow", "rounded", "none", "solid", or array of 8 characters. eg. [ "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" ]
-    row=vim.o.lines,      -- row position
-    col=vim.o.columns,    -- col position
-    width=25,             -- width of the window
-    height=3,             -- height of the window
-    title="Keystrokes",   -- title of the window
-    title_pos="center",   -- title position
+    relative="editor",          -- creates float when specified
+    style="minimal",            -- remove the normal vim setup
+    border="rounded",           -- options: "single", "double", "shadow", "rounded", "none", "solid", or array of 8 characters. eg. [ "╔", "═" ,"╗", "║", "╝", "═", "╚", "║" ]
+    row=vim.o.lines,            -- row position
+    col=vim.o.columns - 10,     -- col position
+    width=25,                   -- width of the window
+    height=3,                   -- height of the window
+    title="Keystrokes",         -- title of the window
+    title_pos="center",         -- title position
   })
 
-  M.window = win
+  M.settings.window = win
 end
 
 -- Toggle the window on and off
@@ -37,18 +38,29 @@ function M.toggle ()
     M.CreateWindow()
   else
     -- nvim_win_close({window}, {force}): boolean
-    vim.api.nvim_win_close(M.window, true)
+    vim.api.nvim_win_close(M.settings.window, true)
   end
 end
 
-function M.setup ()
-  -- Set the default window to 0
-  M.window = 0
+
+function M.setup (config)
+  -- Settings
+  M.settings = {
+    window = 0
+  }
 
   -- Default configuration
   M.config = {
     toggled = false,
+    max_display = 10
   }
+
+  -- Overwrite default configuration with provided config
+  if config then
+    for key, value in pairs(config) do
+      M.config[key] = value
+    end
+  end
 end
 
 return M
