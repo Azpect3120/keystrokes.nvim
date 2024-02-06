1d53ac0bb7b003a4d6c85d4b938af06ff47e0d18
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-02-05 18:51:30 -0700

    Remvoed it

--- lua/keystrokes/init.lua
+++ lua/keystrokes/init.lua
@@ -146,7 +146,6 @@ function M.setup (config)
 
   -- Default configuration
   M.config = {
-    enabled_on_start = false, -- Whether the plugin is enabled on start (default: false)
     toggled = false,    -- Window is toggled (default: false)
     max_display = 5,   -- Maximum number of keystrokes to display (default: 5)
     window = {
@@ -157,12 +156,6 @@ function M.setup (config)
       border = "rounded",   -- Border style (default: "rounded")
     },
   }
-
-  -- Enable the plugin on start if enabled_on_start is true
-  if M.config.enabled_on_start then
-    M.toggle()
-  end
-
 
   -- Overwrite default configuration with provided config
   if config then
